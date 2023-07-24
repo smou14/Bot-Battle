bbdcdce4d11123b8cea44a1c9700c60acaa56895
smou14 <136638687+smou14@users.noreply.github.com>
2023-07-24 11:13:20 +0100

    Add files via upload

--- fight.py
+++ fight.py
@@ -3,24 +3,26 @@ from time import sleep
 from MyBot import MyBot
 from attacker import Attacker
 
+good = "cooperate"
+bad = "defect"
 bot1 = MyBot()
 bot2 = Attacker()
 # Print initial information about each bot
 print(bot1.name + " | " + bot1.description + " | " + bot1.defaultAction)
 print(bot2.name + " | " + bot2.description + " | " + bot2.defaultAction)
 
-#adds the scores
+#adds the scores.
 def evaluate(action1, action2):
-    if action1 == "defend" and action2 == "defend":
+    if action1 == good and action2 == good:
         bot1.score += 2
         bot2.score += 2
-    elif action1 == "attack" and action2 == "defend":
+    elif action1 == bad and action2 == good:
         bot1.score += 3
         bot2.score += 0
-    elif action1 == "defend" and action2 == "attack":
+    elif action1 == good and action2 == bad:
         bot1.score += 0
         bot2.score += 3
-    elif action1 == "attack" and action2 == "attack":
+    elif action1 == bad and action2 == bad:
         bot1.score += 1
         bot2.score += 1
 
@@ -28,7 +30,7 @@ def evaluate(action1, action2):
 for i in range(1,11):
     sleep(0.5)
     evaluate(bot1.action, bot2.action)
-    print(i, "Bot1", bot1.action ,"|" , "Bot2", bot2.action)
+    print(i, "Bot1", bot1.action ,bot1.score ,"|" , "Bot2", bot2.action , bot2.score)
     bot1.respond(bot2.action)
     bot2.respond(bot1.action)
 print(bot1.name ,bot1.score)
